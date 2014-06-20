c27ff9636b7101231a520979532c0301866216de
acgotaku <acgotaku311@gmail.com>
2014-06-20 09:12:52 +0800

    first commit

---
+++ app/js/showAnimation2048.js
@@ -0,0 +1,26 @@
+/**
+ * Created by acgotaku on 14-6-18.
+ */
+function showNumberWithAnimation( i , j , randNumber ){
+
+    var numberCell = $('#number-cell-' + i + "-" + j );
+
+    numberCell.css('background-color',support2048.getNumberBackgroundColor( randNumber ) );
+    numberCell.css('color',support2048.getNumberColor( randNumber ) );
+    numberCell.text( randNumber );
+
+    numberCell.animate({
+        width:"100px",
+        height:"100px",
+        top:support2048.getPosTop( i , j ),
+        left:support2048.getPosLeft( i , j )
+    },50);
+}
+function showMoveAnimation( fromx , fromy , tox, toy ){
+
+    var numberCell = $('#number-cell-' + fromx + '-' + fromy );
+    numberCell.animate({
+        top:support2048.getPosTop( tox , toy ),
+        left:support2048.getPosLeft( tox , toy )
+    },200);
+}
